4dc1217134be830d30ad1885930482797ae6fafc
Jayesh Gadhok <jayeshgadhok@gmail.com>
2023-11-21 17:13:17 +0530

    api update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 
-
-const BASE_URL = 'https://youtube138.p.rapidapi.com/v1';
+const BASE_URL = "https://youtube138.p.rapidapi.com/home/?hl=en&gl=US";
 
 const options = {
-    params: { hl: "en", gl: "US" },
-    headers: {
-        'content-type': 'application/octet-stream',
-        'X-RapidAPI-Key': 'f1ec83dbdamsh6ebfc7358ef1d79p102717jsn24c86fb61859',
-        'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-    }
+	params: {hl: "en", gl: "US"},
+	headers: {
+		"content-type": "application/octet-stream",
+		"X-RapidAPI-Key": "f1ec83dbdamsh6ebfc7358ef1d79p102717jsn24c86fb61859",
+		"X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+	},
 };
 
 export const fetchDataFromApi = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+	const {data} = await axios.get(`${BASE_URL}/${url}`, options);
+	return data;
 };
-
-
